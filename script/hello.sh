5e18f3876726879129df698f731b4007700480b8
Theridelelfo <theridel.elfo@outlook.com>
2026-04-10 21:06:58 +0200

    Add hello.sh script with initial setup messages

---
+++ script/hello.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Questo è il mio primo script bash
+echo "Hello World! Sto preparando l'ambiente per i miei agenti."
+
+# Mostra chi sta eseguendo lo script (per capire se sei root o user)
+echo "Attualmente sto lavorando come l'utente: $(whoami)"
