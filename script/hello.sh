32c1790a55a5fd1d6fc49ac6ee94d4e9c2077d15
Theridelelfo <theridel.elfo@outlook.com>
2026-04-11 16:41:57 +0200

    Modify shebang and add Bash installation notes
    Updated shebang and added installation instructions for Bash in various environments.

--- script/hello.sh
+++ script/hello.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Questo è il mio primo script bash
+# Assicurati che nei tuoi ambiente sia presente Bash. In un Dokerfile:
+
+# alpine	             RUN apk add --no-cache bash
+# ubuntu / debian	     RUN apt-get update && apt-get install -y bash
+# python:slim	         RUN apt-get update && apt-get install -y bash
+
 echo $SHELL
 bash --version
 echo "Hello World! Sto preparando l'ambiente per i miei agenti."
